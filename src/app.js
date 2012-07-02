757fa7e49d0a2e392d0ee17bf2f0b9158d55997d
David Long <dave@davejlong.com>
2012-07-02 19:29:00 -0400

    Added foundation 3.0

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 ---
 ---
 {% include javascripts/jquery.min.js %}
-{% include javascripts/foundation.js %}
+{% include javascripts/jquery.reveal.js %}
+{% include javascripts/jquery.orbit-1.4.0.js %}
+{% include javascripts/jquery.customforms.js %}
+{% include javascripts/jquery.placeholder.min.js %}
+{% include javascripts/jquery.tooltips.js %}
 {% include javascripts/app.js %}
+
+
+  <script src="javascripts/jquery.min.js"></script>
+  <script src="javascripts/jquery.reveal.js"></script>
+  <script src="javascripts/jquery.orbit-1.4.0.js"></script>
+  <script src="javascripts/jquery.customforms.js"></script>
+  <script src="javascripts/jquery.placeholder.min.js"></script>
+  <script src="javascripts/jquery.tooltips.js"></script>
+  <script src="javascripts/app.js"></script>
