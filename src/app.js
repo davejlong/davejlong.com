01b5a751de4b07464edfc7e4e7f925338e378f19
David Long <dave@davejlong.com>
2012-07-06 19:41:36 -0400

    Worked more on laying out blog list

--- src/app.js
+++ src/app.js
@@ -8,11 +8,24 @@
 {% include javascripts/jquery.tooltips.js %}
 {% include javascripts/app.js %}
 
+/* Lazy Load Disqus Comments */
+var disqus_shortname = "{{ site.disqus }}";
+if( $('#disqud_thread').length > 0 ) {
+  var ds_loaded = false,
+      top = $('.tags').offset().top,
+      lazyLoadDisqus = null;
+  lazyLoadDisqus = function () {
+    if( !ds_loaded && $(window).scrollTop() + $(window).height() > top ) {
+      ds_loaded = true;
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.async = true;
+      dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+      ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild(dsq);
+    }
+  };
 
-  <script src="javascripts/jquery.min.js"></script>
-  <script src="javascripts/jquery.reveal.js"></script>
-  <script src="javascripts/jquery.orbit-1.4.0.js"></script>
-  <script src="javascripts/jquery.customforms.js"></script>
-  <script src="javascripts/jquery.placeholder.min.js"></script>
-  <script src="javascripts/jquery.tooltips.js"></script>
-  <script src="javascripts/app.js"></script>
+  $(window).scroll( lazyLoadDisqus );
+  lazyLoadDisqus();
+        
+}
